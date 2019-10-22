cf4d897d82895dbc812fd2f3f38cfeb1af0423b3
Spraxs <jaimy.putter@outlook.com>
2019-10-22 20:16:23 +0200

    See last

--- src/main/java/nl/jaimyputter/server/websocket/modules/task/framework/Task.java
+++ src/main/java/nl/jaimyputter/server/websocket/modules/task/framework/Task.java
@@ -1,9 +1,38 @@
 package nl.jaimyputter.server.websocket.modules.task.framework;
 
+import lombok.Getter;
+import nl.jaimyputter.server.websocket.modules.task.TaskModule;
+
 /**
  * Created by Spraxs
  * Date: 10/22/2019
  */
 
-public class Task {
+public abstract class Task implements Runnable {
+
+    private @Getter int id;
+
+    private @Getter Thread thread;
+
+
+    public Task() {
+        id = TaskModule.Instance.nextId();
+    }
+
+    public void runNewThread() {
+        runNewThread(null);
+    }
+
+    public void runNewThread(String name) {
+        thread = new Thread(this::run);
+
+        if (name != null)
+            thread.setName(name);
+
+        thread.start();
+    }
+
+    public void cancel() {
+
+    }
 }
